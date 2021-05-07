2032b1ef083af2f31bb9c7397e315eed8019df4e
Peter Ryder <peter.ryder2@gmail.com>
2021-05-07 10:51:25 +0100

    first_commit

---
+++ driver.py
@@ -0,0 +1,34 @@
+#%%
+import pandas as pd
+import datetime as datetime
+import os
+
+def CheckBookPrice(pricePoint = None):
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    excelpath = r"C:\Users\ryderp\Documents\projects\amazonWebScrape\excels"
+    path = r"C:\Users\ryderp\Documents\projects\amazonWebScrape\excels\books.xlsx"
+    df = pd.read_excel(path)
+    df["checked"] = None
+    
+    for index,row in df.iterrows():
+        print(f"Current price is {row['price_USD']}")
+        if row["price_USD"] < pricePoint:
+            print(f"Price is below price point \n\n {row}")
+            df.loc[index, 'checked']  = "X"
+            newFileName = f"new_book_{now}.xlsx"
+            neFilePath = os.path.join(excelpath,newFileName )
+            df.to_excel(neFilePath, index = False )
+
+            return row.to_dict() 
+    return None
+
+
+pricePoint =  50.40
+bookCheck = CheckBookPrice(pricePoint=pricePoint)
+if bookCheck == None:
+    print("no books of interest")
+else:
+    print(f" There is a book of interest \n\n\n {bookCheck}")
+exit()
+
+# %%
